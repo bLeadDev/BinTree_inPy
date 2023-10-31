096ea88eeaacb2fa434ca2dae37c1f9e23c8c70f
bLeadDev <59748268+bLeadDev@users.noreply.github.com>
2023-10-31 10:27:32 +0000

    Changed settings

---
+++ binTree.py
@@ -0,0 +1,17 @@
+
+class BinTree:
+    """Damn linter"""
+
+    def __init__(self):
+        """For the goddamn linter"""
+        data = [[]]
+        hashmap = [];
+        
+    def add(self, number):
+        """"For the damn linter"""
+        hash_val = hash(number)
+        
+
+    def contains(self, number):
+        """for the damn Linter!"""
+        
